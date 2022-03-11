79aa76a10c493fd6754e0ded7b2dd12eafa50e5b
chungisugithub <chungisu@yahoo.com>
2022-03-11 11:02:48 +0900

    2차원배열-다이어몬드출력

---
+++ 다이어몬드.cpp
@@ -0,0 +1,57 @@
+﻿#include <stdio.h>
+
+//다이아몬드의 크기를 지정
+//"홀수"만 입력하되 19미만이 보기 좋음
+
+#define MAX 5
+
+int main()
+{
+	int row, col, k(0);		//row=행, col=열
+	int begin = 0; int end = 0;
+	int center = MAX / 2;
+	int arr[MAX][MAX] = { 0 };
+
+	//대입
+	for (row = 0; row < MAX; row++)
+	{
+		//상
+		if (row <= center)
+		{
+			begin = center - row;
+			end = row + center;
+		}
+
+		//하
+		if (row > center)
+		{
+			begin = row - center;
+			//끝 값이 MAX의 크기에 비례하여 대입
+			end = (MAX + center - 1) - row;
+		}
+
+		//상하에 따라 대입
+		for (col = begin; col <= end; col++)
+		{
+			++k;
+			arr[row][col] = k;
+		}
+	}
+
+	//출력
+	for (row = 0; row < MAX; row++)
+	{
+		for (col = 0; col < MAX; col++)
+		{
+			if(arr[row][col] != 0) 
+				printf("%4d", arr[row][col]);
+			else 
+				printf("    ");
+		}
+		printf("\n");
+	}
+
+	printf("\n");
+
+	return 0;
+}
